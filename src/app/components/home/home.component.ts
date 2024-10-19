f58af3dc6649ea375f442c87ed473d1036e4b4d6
israel belete <ibelete2000@gmail.com>
2024-10-19 09:01:43 -0600

    added homepagennavs

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { FooterComponent } from "../../footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+    encapsulation: ViewEncapsulation.None  // Add this line to disable encapsulation
+
 })
 export class HomeComponent {
 
